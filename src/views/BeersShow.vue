fbef2f687f7920ae220c6f1f95a07aa7127dcc91
Gail Eddy <gaileddy68@gmail.com>
2021-12-22 09:52:55 -0600

    adds style to beer show page

--- src/views/BeersShow.vue
+++ src/views/BeersShow.vue
@@ -3,15 +3,24 @@
     <div class="container">
       <div class="section-title">
         <h1>{{ beer.name }}</h1>
-        <img v-bind:src="beer.image_url" :alt="beer.name" />
-        <!-- <img v-bind:src="recipe.image_url" :alt="recipe.title" /> -->
-        <p>Style: {{ beer.style }}</p>
-        <p>Description: {{ beer.description }}</p>
-        <router-link v-bind:to="`/recipes/${beer.recipes[0].id}`">
-          <p>Recipe Pairing: {{ beer.recipes[0].title }}</p>
-        </router-link>
+        <div class="d-flex">
+          <img class="show-image" v-bind:src="beer.image_url" :alt="beer.name" />
+          <div class="d-flex flex-wrap flex-row flex-sm-column justify-content-between-show-content">
+            <p>
+              <span class="gold">Style:</span>
+              {{ beer.style }}
+            </p>
+            <p>
+              <span class="gold">Description:</span>
+              {{ beer.description }}
+            </p>
+            <router-link v-bind:to="`/recipes/${beer.recipes[0].id}`">
+              <p>Recipe Pairing: {{ beer.recipes[0].title }}</p>
+            </router-link>
 
-        <router-link to="/beers">Back to all beers</router-link>
+            <router-link to="/beers">Back to all beers</router-link>
+          </div>
+        </div>
       </div>
     </div>
   </section>
